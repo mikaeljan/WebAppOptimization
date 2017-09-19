04aaa632fd3a7a6b8f90f71e544f6783d9273a58
Michal Janek <mikael.janek@gmail.com>
2017-09-19 14:32:00 +0200

    add gzip

---
+++ README.md
@@ -0,0 +1,9 @@
+# Performance_Optimization
+Apply the skills learned for optimization of graphics, HTML, CSS, and JavaScript to take a poorly optimized site and reduce the total load size to under 1.3mb and total requests to less than 27. Students will need to analyze the assets they've been provided to see where optimizations can and need to be made, make those optimizations, and measure their success. Multiple analyzation, optimization, and measuring cycles may be required.
+
+## Notes
+1. Inline css?
+2. various img for sizing? - media queries/crop images
+3. CDN or file
+4. minify HTML- pridat k inline source task
+5. Gzip
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const gulp  = require('gulp'),
+    uglify    = require('gulp-uglify'),
+    useref    = require('gulp-useref'),
+    gulpIf    = require('gulp-if'),
+    del       = require('del'),
+    imagemin  = require('gulp-imagemin'),
+    csso      = require('gulp-csso'),
+    gzip      = require('gulp-gzip'),
+    // htmlmin   = require('gulp-htmlmin'),
+    inlineSource = require('gulp-inline-source');
+
+const options = {
+    src: 'src',
+    dist: 'dist'
+};
+
+gulp.task('inlineSource',["useref"],function(){
+    return gulp.src(options.dist+'/index.html')
+        .pipe(inlineSource())
+        .pipe(gulp.dest(options.dist));
+});
+
+// Useref task + minifying CSS and JS
+gulp.task('useref',["images"], function(){
+    return gulp.src(options.src +'/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JS file
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', csso()))
+        .pipe(gzip())
+        .pipe(gulp.dest(options.dist));
+});
+
+
+//Images task, compression and moving
+gulp.task('images',[], function() {
+    return gulp.src(options.src + '/img/**/*+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest(options.dist + '/img'))
+});
+gulp.task('clean', function() {
+  del([options.dist, options.src+'/css/styles.min.css',options.src+'/js/main.min.js',]);
+});
+
+gulp.task("build", ['inlineSource'], function() {
+  return gulp.src([],{ base: './'}).
+  pipe(gulp.dest(options.dist));
+});
+
+gulp.task("default", ["clean"], ()=>{
+  gulp.start('build');
+});
